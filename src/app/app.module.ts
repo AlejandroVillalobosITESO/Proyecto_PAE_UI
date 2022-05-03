6ebb4e22249610d9dac5daefd765f2880a57a645
Alejandro Villalobos <is728135@iteso.mx>
2022-05-02 19:08:33 -0600

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
@@ -12,6 +11,11 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginSuccesComponent } from './pages/login-succes/login-succes.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ForumComponent } from './pages/forum/forum.component';
+import { NewForumComponent } from './pages/new-forum/new-forum.component';
+import { PostComponent } from './pages/post/post.component';
+import { NewPostComponent } from './pages/new-post/new-post.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +25,12 @@ import { LoginSuccesComponent } from './pages/login-succes/login-succes.componen
     HomeComponent,
     LoginComponent,
     RegisterComponent,
-    LoginSuccesComponent
+    LoginSuccesComponent,
+    ProfileComponent,
+    ForumComponent,
+    NewForumComponent,
+    PostComponent,
+    NewPostComponent
   ],
   imports: [
     BrowserModule,
